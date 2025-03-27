bd26fc03b1985fed97eba64fb3e904af7f7b2a43
kriskriskris23 <161980849+kriskriskris23@users.noreply.github.com>
2025-03-27 15:16:32 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyDI_fGu98sgzr8ie4DphTFFkApEbwwdSyk",
+    authDomain: "blessmemes.firebaseapp.com",
+    projectId: "blessmemes",
+    storageBucket: "blessmemes.firebasestorage.app",
+    messagingSenderId: "647948484551",
+    appId: "1:647948484551:web:db884bd3346d838737e3e2",
+    measurementId: "G-0GY321M1ML"
+};
