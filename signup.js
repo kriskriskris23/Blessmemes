58fd5f3d16c0f8f9bbfacbe06267fa661fe217ee
kriskriskris23 <161980849+kriskriskris23@users.noreply.github.com>
2025-03-27 15:21:56 +0800

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,15 +1,7 @@
+// signup.js
+import { firebaseConfig } from './firebase-config.js';
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDI_fGu98sgzr8ie4DphTFFkApEbwwdSyk",
-    authDomain: "blessmemes.firebaseapp.com",
-    projectId: "blessmemes",
-    storageBucket: "blessmemes.firebasestorage.app",
-    messagingSenderId: "647948484551",
-    appId: "1:647948484551:web:db884bd3346d838737e3e2",
-    measurementId: "G-0GY321M1ML"
-};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
